39cba3497c0f3090cdd0d254b26d093b040484f7
Atsumi3 <atsumi@mail.bizen.jp>
2021-04-22 20:09:32 +0900

    text channel to PlatformText

--- android/app/src/main/kotlin/jp/bizen/flutter/practice/channel/platform/PlatformText.kt
+++ android/app/src/main/kotlin/jp/bizen/flutter/practice/channel/platform/PlatformText.kt
@@ -1,20 +1,20 @@
 package jp.bizen.flutter.practice.channel.platform
 
 import android.content.Context
-import jp.bizen.flutter.practice.channel.ui.main.MainActivity
-import jp.bizen.flutter.practice.channel.ui.textinput.TextInputActivity
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import jp.bizen.flutter.practice.channel.ui.textinput.TextInputActivity
 
 class PlatformText private constructor(private val channel: MethodChannel) {
     companion object {
         lateinit var instance: PlatformText
+        private const val TEXT_CHANNEL = "channel/text"
         private const val METHOD_LAUNCH_TEXT_SCREEN = "LaunchTextScreen"
         private const val METHOD_SEND_TEXT = "SendText"
 
         fun init(messenger: BinaryMessenger, context: Context) {
-            val textChannel = MethodChannel(messenger, MainActivity.TEXT_CHANNEL)
+            val textChannel = MethodChannel(messenger, TEXT_CHANNEL)
             instance = PlatformText(textChannel)
             textChannel.setMethodCallHandler { call, result ->
                 instance.handler(context, call, result)
--- android/app/src/main/kotlin/jp/bizen/flutter/practice/channel/ui/main/MainActivity.kt
+++ android/app/src/main/kotlin/jp/bizen/flutter/practice/channel/ui/main/MainActivity.kt
@@ -6,10 +6,6 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
-    companion object {
-        const val TEXT_CHANNEL = "channel/text"
-    }
-
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
